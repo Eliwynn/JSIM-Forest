efcb06ec7c5819982eade471fa5e013ac174d8d4
Flavian D <dutoitflavian@gmail.com>
2019-03-12 20:03:28 +0100

    Maj Mardi

---
+++ src/controller/Cell.java
@@ -0,0 +1,89 @@
+package controller;
+
+import controller.*;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+public class Cell extends Pane {
+	
+	private int x;
+	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	private int y;
+	private int type = 0;
+	
+	Background bg_white = new Background(new BackgroundFill(Color.WHITE, null, null));
+	Background bg_lightgreen = new Background(new BackgroundFill(Color.LIGHTGREEN, null, null));
+	Background bg_green = new Background(new BackgroundFill(Color.DARKSEAGREEN, null, null));
+	Background bg_darkgreen = new Background(new BackgroundFill(Color.DARKGREEN, null, null));
+
+	public Cell(int posX, int posY) {
+		this.x = posX;
+		this.y = posY;
+		
+		this.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+		this.setBackground(bg_white);
+		
+		this.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+            	if (type > 2) {
+            		type = 0;
+            		changeColor();
+            		System.out.println("X : " + posX + " Y : " + posY + " Type : " + type);
+            	}
+            	else {
+            		type++;
+            		changeColor();
+            		System.out.println("X : " + posX + " Y : " + posY + " Type : " + type);
+            	}
+            }
+        });
+	}
+	
+	public void changeColor() {
+		switch (type) {
+		case 0:	this.setBackground(bg_white);
+		break;
+		case 1:	this.setBackground(bg_lightgreen);
+		break;
+		case 2:	this.setBackground(bg_green);
+		break;
+		case 3:	this.setBackground(bg_darkgreen);
+		break;
+	}
+	}
+}
--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,9 +1,11 @@
 package controller;
 
+import controller.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import application.*;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -11,6 +13,7 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.ColumnConstraints;
@@ -22,22 +25,33 @@ import javafx.scene.paint.Color;
 public class Controller {
 
 	@FXML
-//	private GridPane grid;
 	public Button butbut;
+	public AnchorPane anchor;
 	public GridPane gridgrid;
+	
+    int rows = 20;
+    int columns = 20;
+	
 	
 	@FXML
 	public void initialize() {
-		this.butbut.setText("Patrick fdp");
-		this.gridgrid.setGridLinesVisible(true);
+		
+		setWindow();
 		setGrid();
+		setCells();
+
 	}
 
 	@FXML
 	public void setButtonText(String text) {
 		butbut.setText(text);
 	}
-
+	
+	@FXML
+	public void setWindow() {
+		this.butbut.setText("Reset");
+	}
+	
 	@FXML
 	public void setGrid() {
 		
@@ -45,46 +59,52 @@ public class Controller {
         int preferredHeight = 100;
         int maxHeight = 110;
         
-        int rows = 20;
-        int columns = 20;
+        this.gridgrid.getChildren().clear();
 		
-		RowConstraints con = new RowConstraints(minHeight, preferredHeight, maxHeight);
-		ColumnConstraints col = new ColumnConstraints(minHeight, preferredHeight, maxHeight);
-		
-		Pane pane1 = new Pane();
-		Pane pane2 = new Pane();
-		Background bg = new Background(new BackgroundFill(Color.DARKGREEN, null, null));
-		pane1.setBackground(bg);
-		pane2.setBackground(bg);
-		
+        gridgrid.setGridLinesVisible(true);
+        
 		gridgrid.getRowConstraints().remove(0);
+		gridgrid.getColumnConstraints().remove(0);
 		
 		for (int i = 0; i <= rows; i++) {
-			gridgrid.getRowConstraints().add(con);
+			gridgrid.getRowConstraints().add(new RowConstraints(minHeight, preferredHeight, maxHeight));
 		}
 		
 		for (int i = 0; i <= columns-1; i++) {
-			gridgrid.getColumnConstraints().add(col);
+			gridgrid.getColumnConstraints().add(new ColumnConstraints(minHeight, preferredHeight, maxHeight));
 		}
-		
-		gridgrid.add(pane1, 0, 0);
-		gridgrid.add(pane2, 5, 6);
 
-		
 	}
 	
 	@FXML
-    public void mouseEntered(MouseEvent e) {
-        Node source = (Node)e.getSource() ;
-        Integer colIndex = GridPane.getColumnIndex(source);
-        //Integer rowIndex = GridPane.getRowIndex(source);
-        //System.out.printf("Mouse entered cell [%d, %d]%n", colIndex.intValue(), rowIndex.intValue());
-		System.out.println(colIndex.intValue());
-    }
+	public void setCells() {
+		
+		for (int i = 0; i <= rows; i++) {
+			for (int j = 0; j <= columns; j++) {
+				gridgrid.add(new Cell(i,j), i, j);
+			}
+		}
+	}
 	
 	@FXML
-	public void testButton() {
-		System.out.printf("Mon cul c'est du polystyrene");
+    public void coordCheck(MouseEvent e) {
+        //Node source = (Node)e.getSource() ;
+        //Integer colIndex = GridPane.getColumnIndex(source);
+        //Integer rowIndex = GridPane.getRowIndex(source);
+        //System.out.println(GridPane.getColumnIndex(source));
+		
+//		Node source = (Node)e.getSource();
+//        Integer colIndex = (GridPane.getColumnIndex(source) == null) ?  0 : (GridPane.getColumnIndex(source));
+//        Integer colRow = (GridPane.getRowIndex(source) == null) ? 0 : (GridPane.getRowIndex(source));
+//        System.out.println(colIndex + " : " + colRow);
+    }
+	
+
+	
+	@FXML
+	public void resetButton() {
+		
+		
 	}
 	
 	
